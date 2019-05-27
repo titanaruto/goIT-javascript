72c652d35847fdcd9c3948bdfba86a27bce56737
Aleksander <titanaruto@gmail.com>
2019-05-27 14:47:21 +0300

    add module 10

---
+++ module10/src/js/workWithItem.js
@@ -0,0 +1,27 @@
+import {createListItem} from "./createContent";
+
+export const renderListItems = (listRef, data) => {
+    const listItems = data.map(item => createListItem(item));
+    listRef.innerHTML = '';
+    listRef.append(...listItems);
+};
+export const addListItem = (listRef, note) => {
+    listRef.append(createListItem(note));
+};
+
+export const validOrInvalidShow = (obj) => {
+    obj.forEach((val) => {
+        if (val.value !== "") {
+            val.classList.remove("invalid");
+            val.classList.add("valid");
+        } else {
+            val.classList.remove("valid");
+            val.classList.add("invalid");
+        }
+    });
+};
+export const removeClassAndValue = (obj) => {
+    obj.value = "";
+    obj.classList.remove("valid");
+};
+
